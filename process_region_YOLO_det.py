c4359d31c7d19bc3bb7ce2e0c95a5cc150ae834f
Kevin <Kevin>
2025-06-25 11:04:15 -0700

    correction factor removal

--- process_region_YOLO_det.py
+++ process_region_YOLO_det.py
@@ -9,10 +9,6 @@ def fix_region(region, tile_size):
     reg['height'] = max(reg['height'], tile_size)
     return reg
 
-# Log-based correction (more forgiving for small # of tiles)
-def correction_factor(n_tiles, corr_fac):
-    return max(1.0 - corr_fac * np.log1p(n_tiles), 0.5)
-
 def process_region(region, **kwargs):
 
     metadata = kwargs['metadata']
@@ -130,18 +126,9 @@ def process_region(region, **kwargs):
         num_pos += num_pos_curr
         num_pos_neg += num_pos_curr + torch.sum(r.boxes.cls == 1).cpu().numpy()
 
-
-    try:
-        _correction_factor = float(kwargs['additional_configs'].get('correction_factor', 0.15))
-    except:
-        _correction_factor = 0.15
-    num_neg = num_pos_neg - num_pos
-    num_neg = int(num_neg * correction_factor(len(slices), _correction_factor))
-    positivity = num_pos / (num_pos + num_neg) * 100 if (num_pos + num_neg) > 0 else 0
-
-    text = '(+) {:.2f} %\n'.format(positivity)
+    text = '(+) {:.2f} %\n'.format(num_pos / num_pos_neg * 100 if num_pos_neg > 0 else 0)
     text += '(+) cells: {}\n'.format(num_pos)
-    text += '(-) cells: {}\n'.format(num_neg)
+    text += '(-) cells: {}\n'.format(num_pos_neg - num_pos)
     metrics = {
         "mib_pos":   int(num_pos),
         "mib_total": int(num_pos_neg),
